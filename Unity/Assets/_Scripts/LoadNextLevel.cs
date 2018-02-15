a1c717f83e5ef1ae1a21cf7e238983f1ebb9e0ca
Luca Ferraris <paraesmic@hotmail.it>
2018-02-15 15:57:38 +0100

    tentativo2

---
+++ Unity/Assets/_Scripts/LoadNextLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNextLevel : MonoBehaviour {
+
+	public GameManager GameManager;
+
+	public void LoadScene(int level)
+	{	
+		Application.LoadLevel(level);
+	}
+}
